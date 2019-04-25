63f1c88bb4f362b748d99c744abe5502cfbae4dd
themanyone <nospam@thenerdshow.com>
2019-04-24 21:33:21 -0800

    add social icons

--- start.php
+++ start.php
@@ -8,9 +8,16 @@
 function theme_fails_us_init() {
     // change the default header_logo
     elgg_register_plugin_hook_handler('view', 'page/elements/header_logo', 'logo_fails_us');
+    elgg_register_plugin_hook_handler('view', 'page/elements/body', 'header_fails_us');
     elgg_register_css('fails.us.css', elgg_get_simplecache_url('css/fails.us.css'));
 }
 
+function header_fails_us($hook, $type, $vars, $params) {
+   $script = '<div class="elgg-heading" style="position:absolute;top:55px;right:5px;"><script src="https://thenerdshow.com/social/mediaIcons.js?button0=&amp;button1=&amp;button2=&amp;button3=&amp;button4=&amp;button5=&amp;button12=&amp;button13=&amp;url13=//youtube.com/themanyone"></script></div>';
+   // $vars = preg_replace('#(<div.*?header">)(.*?)(</div>)#',"$1 $script $2$3", $vars);
+   $vars = $script.$vars;
+   return $vars;
+}
 function logo_fails_us($hook, $type, $vars, $params) {
    // change default site colors
    elgg_load_css('fails.us.css');
